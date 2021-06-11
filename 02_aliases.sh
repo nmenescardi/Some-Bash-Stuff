2a84d4bd675ca09b12f8c221b41d21289921d693
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-06-11 18:16:06 -0300

    add aliases

---
+++ 02_aliases.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+exec >> ~/.bash_aliases
+exec 2> /dev/null
+
+cat << EOF
+alias dcr='docker-compose run --rm'
+alias dc='docker-compose'
+alias lsa='ls -alh'
+alias ll='lsa'
+alias cb='xclip -sel clip'
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+alias cls='clear'
+alias grep='grep --color=auto'
+alias egrep='egrep --color=auto'
+alias fgrep='fgrep --color=auto'
+alias h='history'
+alias j='jobs -l'
+alias ports='netstat -tulanp'
+
+# confirmation #
+alias mv='mv -i'
+alias cp='cp -i'
+alias ln='ln -i'
+EOF
