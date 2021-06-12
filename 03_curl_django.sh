408857a4bc448ceecafde36df469360016d1d7aa
Nicolas Menescardi <nicolasmenescardi@hotmail.com>
2021-06-11 23:47:23 -0300

    saving pairs - improve user output

--- 03_curl_django.sh
+++ 03_curl_django.sh
@@ -4,29 +4,31 @@ YOUR_PASS=''
 COOKIES=cookies.txt
 CURL_BIN="curl -s -c $COOKIES -b $COOKIES -e $LOGIN_URL"
 
-echo -n "Django Auth: get csrftoken ..."
+printf "1 - Django Auth: getting csrftoken. \n"
 $CURL_BIN $LOGIN_URL > /dev/null
 DJANGO_TOKEN="csrfmiddlewaretoken=$(grep csrftoken $COOKIES | sed 's/^.*csrftoken\s*//')"
 
-echo -n " perform login ..."
+printf "2 - Performing login. \n"
 $CURL_BIN \
     -d "$DJANGO_TOKEN&username=$YOUR_USER&password=$YOUR_PASS" \
     -X POST $LOGIN_URL
 
-echo -n " do something while logged in ..."
+printf "3 - Getting new token from form page html. \n"
 FORM_HTML=$($CURL_BIN \
     -H "$DJANGO_TOKEN&..." \
     -X GET https://vwap-offset.herokuapp.com/save/config/input/)
 
-#echo $FORM_HTML
-
 # Getting form token
-DJANGO_TOKEN_2=$(echo "$FORM_HTML" | grep csrfmiddlewaretoken | sed 's/^.*value\s*//' | sed 's/^.//;s/.$//' | sed 's/^.//;s/.$//')
-echo $DJANGO_TOKEN_2
+DJANGO_TOKEN_2="csrfmiddlewaretoken=$(echo "$FORM_HTML" | grep csrfmiddlewaretoken | sed 's/^.*value\s*//' | sed 's/^.//;s/.$//' | sed 's/^.//;s/.$//')"
 
+# Read config file
+FILE_CONTENT=$(cat varPairs.json)
 
+printf "4 - Saving Config. \n"
+$CURL_BIN \
+    -d "$DJANGO_TOKEN_2&file_content=$FILE_CONTENT" \
+    -X POST https://vwap-offset.herokuapp.com/save/config/
 
-
-
-echo " logout"
+printf "5 - Logout \n"
+# Cleanup
 rm $COOKIES
